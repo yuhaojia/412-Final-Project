884ca1d296701d6a58cfce493d60aa28839d4e48
john <zc15@illinois.edu>
2018-04-29 21:01:34 -0500

    fill null in two different ways

--- data_info.py
+++ data_info.py
@@ -1,23 +1,86 @@
 import pandas as pd
 import numpy as np
+import sklearn.feature_selection
+from sklearn.preprocessing import Imputer
 
 
+pd.options.display.max_rows = 100
+data = pd.read_csv('testing.csv')
+data = data.drop(['Id'], 1)
+data = pd.get_dummies(data, columns=['Product_Info_2'], drop_first=True)
+print(data.nunique())
+count = data.nunique()
+cat_list = list()
+num_list = list()
+for i in range(len(count)):
+    if count[i] > 3:
+        num_list.append(i)
+    else:
+        cat_list.append(i)
+print(num_list)
+print(cat_list)
+col_name = list(data)
+num_col = []
+cat_col = []
+for elem in num_list:
+    num_col.append(col_name[elem])
+for elem in cat_list:
+    cat_col.append(col_name[elem])
 
-data = pd.read_csv('training.csv')
+df = data.loc[:, num_col]
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+df = pd.DataFrame(imp.fit_transform(df), columns=num_col)
+df2 = data.loc[:, cat_col]
+imp2 = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+df2 = pd.DataFrame(imp2.fit_transform(df2), columns=cat_col)
 
-# print(data.shape)
+new_data = df.join(df2)
+
+print(new_data.shape, df.shape, df2.shape)
+print(new_data.columns)
+# print(data.head())
+# col_name = data.describe().columns
+# print(col_name)
+# i = col_name.get_loc('Medical_Keyword_1')
+# print(i)
+# data['Medical_Keyword_9'].fillna(20, inplace=True)
+# # data[col_name.get_loc("Medical_Keyword_1"):col_name.get_loc("Medical_Keyword_48")].fillna(2, inplace=True)
+# #
+# print(data.iloc[1:20, col_name.get_loc("Medical_Keyword_1"):col_name.get_loc("Medical_Keyword_48")])
 #
-# print(data.Product_Info_2.value_counts())
+# print(data.select_dtypes(include=['float64']))
+# describe = data.describe(include='all')
+# print(data.groupby['Product_Info_1'].size())
+
+# with open("describe.txt", 'w') as f:
+#     for d in describe:
+#         f.write(str(d))
+# # print(data.shape)
+# print(data.describe())
+# print(data.Response.value_counts())
+# missing = data.isnull().sum(axis=1).to_frame('nulls')
+# print(missing.sort_values(by=['nulls']))
+
+# row, col = data.shape
+# drop_list = list()
+# for i in range(col):
+#     if data.iloc[:, i].isnull().sum() > row * 0.3:
+#         drop_list.append(i)
+# data.drop(data.columns[drop_list], axis=1, inplace=True)
 #
-print(data.Response.value_counts())
+# row, col = data.shape
+# drop_list = list()
+# for i in range(row):
+#     if data.iloc[i, :].isnull().sum() > col * 0.1:
+#         drop_list.append(i)
+# data.drop(data.index[drop_list], axis=0, inplace=True)
 
-# product_info = pd.get_dummies(data.Product_Info_2)
-# data = pd.get_dummies(data, columns=['Product_Info_2'], drop_first=True)
-#
-# print(data.shape)
-
-test = pd.read_csv('testing.csv')
-
+# sel = sklearn.feature_selection.VarianceThreshold(threshold=(.8 * (1 - .8)))
+# X = sel.fit_transform(data)
+# print(X.shape)
+# test = pd.read_csv('testing.csv')
+# missing = test.isnull().sum().to_frame('nulls')
+# print(missing.sort_values(by=['nulls']))
 # print(test.shape)
 #
 # print(test.Product_Info_2.value_counts())
--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from sklearn import multiclass
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import Imputer, LabelEncoder, StandardScaler
+from sklearn.preprocessing import Imputer, LabelEncoder, StandardScaler, scale, MinMaxScaler, QuantileTransformer, RobustScaler
+from sklearn.model_selection import KFold
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, chi2
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import accuracy_score
 import pickle
 import csv
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.utils import np_utils
-import tensorflow as tf
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout
+# from keras.utils import np_utils
+# import tensorflow as tf
 from npy_to_csv import npy2csv
 
 
@@ -27,18 +30,96 @@ def prob_to_class(prob):
 
 data = pd.read_csv('training.csv')
 data = data.drop(['Id'], 1)
-
 y = data.pop('Response')
-
-X = pd.get_dummies(data, columns=['Product_Info_2'], drop_first=True)
 # X = data.drop(['Product_Info_2'], 1)
 # Pre-process data
-imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imp.fit(X)
-X = imp.transform(X)
 
-# X = data.values[:, :-1]
-X = StandardScaler().fit(X).transform(X)
+# data['Product_Info_1'].fillna(1, inplace=True)
+# data['Product_Info_3'].fillna(26, inplace=True)
+# data['Product_Info_5'].fillna(2, inplace=True)
+# data['Product_Info_6'].fillna(3, inplace=True)
+# data['Product_Info_7'].fillna(1, inplace=True)
+# data['Medical_Keyword_1'].fillna(0, inplace=True)
+# data['Medical_Keyword_2'].fillna(0, inplace=True)
+# data['Medical_Keyword_3'].fillna(0, inplace=True)
+# data['Medical_Keyword_4'].fillna(0, inplace=True)
+# data['Medical_Keyword_5'].fillna(0, inplace=True)
+# data['Medical_Keyword_6'].fillna(0, inplace=True)
+# data['Medical_Keyword_7'].fillna(0, inplace=True)
+# data['Medical_Keyword_8'].fillna(0, inplace=True)
+# data['Medical_Keyword_9'].fillna(0, inplace=True)
+# data['Medical_Keyword_10'].fillna(0, inplace=True)
+# data['Medical_Keyword_11'].fillna(0, inplace=True)
+# data['Medical_Keyword_12'].fillna(0, inplace=True)
+# data['Medical_Keyword_13'].fillna(0, inplace=True)
+# data['Medical_Keyword_14'].fillna(0, inplace=True)
+# data['Medical_Keyword_15'].fillna(0, inplace=True)
+# data['Medical_Keyword_16'].fillna(0, inplace=True)
+# data['Medical_Keyword_17'].fillna(0, inplace=True)
+# data['Medical_Keyword_18'].fillna(0, inplace=True)
+# data['Medical_Keyword_19'].fillna(0, inplace=True)
+# data['Medical_Keyword_20'].fillna(0, inplace=True)
+# data['Medical_Keyword_21'].fillna(0, inplace=True)
+# data['Medical_Keyword_22'].fillna(0, inplace=True)
+# data['Medical_Keyword_23'].fillna(0, inplace=True)
+# data['Medical_Keyword_24'].fillna(0, inplace=True)
+# data['Medical_Keyword_25'].fillna(0, inplace=True)
+# data['Medical_Keyword_26'].fillna(0, inplace=True)
+# data['Medical_Keyword_27'].fillna(0, inplace=True)
+# data['Medical_Keyword_28'].fillna(0, inplace=True)
+# data['Medical_Keyword_29'].fillna(0, inplace=True)
+# row, col = data.shape
+# drop_list = list()
+# for i in range(col):
+#     if data.iloc[:, i].isnull().sum() > row * 0.5:
+#         drop_list.append(i)
+# data.drop(data.columns[drop_list], axis=1, inplace=True)
+
+# row, col = data.shape
+# drop_list_tmp = list()
+# for i in range(row):
+#     if data.iloc[i, :].isnull().sum() > col * 0.1:
+#         drop_list_tmp.append(i)
+# data.drop(data.index[drop_list_tmp], axis=0, inplace=True)
+
+data = pd.get_dummies(data, columns=['Product_Info_2'], drop_first=True)
+
+count = data.nunique()
+cat_list = list()
+num_list = list()
+for i in range(len(count)):
+    if count[i] > 3:
+        num_list.append(i)
+    else:
+        cat_list.append(i)
+print(num_list)
+print(cat_list)
+col_name = list(data)
+num_col = []
+cat_col = []
+for elem in num_list:
+    num_col.append(col_name[elem])
+for elem in cat_list:
+    cat_col.append(col_name[elem])
+
+df = data.loc[:, num_col]
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+df = pd.DataFrame(imp.fit_transform(df), columns=num_col)
+df2 = data.loc[:, cat_col]
+imp2 = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+df2 = pd.DataFrame(imp2.fit_transform(df2), columns=cat_col)
+
+X = df.join(df2)
+
+# sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
+
+# scaler = StandardScaler()
+scaler = MinMaxScaler()
+# scaler = RobustScaler()
+# scaler = QuantileTransformer(random_state=0)
+X = scaler.fit_transform(X)
+# sel = SelectKBest(chi2, k=100)
+# X = sel.fit_transform(X, y)
 print(X.shape, y.shape)
 
 
@@ -48,11 +129,11 @@ print(X.shape, y.shape)
 # dummy_y = np_utils.to_categorical(encoded_Y)
 
 # Built model
-# model = linear_model.LogisticRegression(C=1e5)
+model = linear_model.LogisticRegression(C=1e5)
 # model = linear_model.LinearRegression()
 # model = multiclass.OneVsRestClassifier(estimator=svm.SVC(random_state=0, kernel='linear'))
 # model = DecisionTreeClassifier(random_state=0)
-model = RandomForestClassifier(max_depth=8, n_estimators=10, random_state=0)
+# model = RandomForestClassifier(max_depth=8, n_estimators=10, random_state=0)
 # model = MLPClassifier(solver="sgd", hidden_layer_sizes=(3,), activation="")
 # model = KNeighborsClassifier(n_neighbors=5)
 
@@ -78,14 +159,17 @@ model = RandomForestClassifier(max_depth=8, n_estimators=10, random_state=0)
 
 
 # Fit model
-# model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-# model.fit(X, dummy_y, validation_split=0.1, epochs=150, batch_size=64, shuffle=True)
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.fit(X, dummy_y, validation_split=0.1, epochs=50, batch_size=64, shuffle=True)
 model.fit(X, y)
 
 
+# Score
+print(cross_val_score(model, X, y, cv=5))
+
 # Save model
-filename = 'random_forest_model.sav'
-pickle.dump(model, open(filename, 'wb'))
+# filename = 'random_forest_model.sav'
+# pickle.dump(model, open(filename, 'wb'))
 # model.save('MLP_model.h5')
 # Load model
 # model = pickle.load(open(filename, 'rb'))
@@ -93,17 +177,62 @@ pickle.dump(model, open(filename, 'wb'))
 # Predict
 test_data = pd.read_csv('testing.csv')
 index = test_data.pop('Id')
+# test_data['Product_Info_1'].fillna(1, inplace=True)
+# # test_data['Product_Info_3'].fillna(26, inplace=True)
+# test_data['Product_Info_5'].fillna(2, inplace=True)
+# test_data['Product_Info_6'].fillna(3, inplace=True)
+# test_data['Product_Info_7'].fillna(1, inplace=True)
+# test_data['Medical_Keyword_1'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_2'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_3'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_4'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_5'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_6'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_7'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_8'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_9'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_10'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_11'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_12'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_13'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_14'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_15'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_16'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_17'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_18'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_19'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_20'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_21'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_22'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_23'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_24'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_25'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_26'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_27'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_28'].fillna(0, inplace=True)
+# test_data['Medical_Keyword_29'].fillna(0, inplace=True)
+# test_data.drop(test_data.columns[drop_list], axis=1, inplace=True)
 test_data = pd.get_dummies(test_data, columns=['Product_Info_2'], drop_first=True)
 # test_data = test_data.drop(['Product_Info_2'], 1)
-test_data = imp.transform(test_data)
-test_data = StandardScaler().fit(test_data).transform(test_data)
+
+df = test_data.loc[:, num_col]
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+df = pd.DataFrame(imp.fit_transform(df), columns=num_col)
+df2 = test_data.loc[:, cat_col]
+imp2 = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+df2 = pd.DataFrame(imp2.fit_transform(df2), columns=cat_col)
+
+test_data = df.join(df2)
+
+# test_data = imp.transform(test_data)
+test_data = scaler.transform(test_data)
+# test_data = sel.transform(test_data)
 print(test_data.shape)
 
 # res = model.predict_proba(test_data)
 # res = prob_to_class(res)
 
 res = model.predict(test_data)
-print(res[0:5])
-# res = np.argmax(res, axis=1)
-np.save('result', res)
-npy2csv('result.npy', "result"+str(np.random.randint(0, 1000))+".csv")
+
+# np.save('result', res)
+# npy2csv('result.npy', "result"+str(np.random.randint(0, 1000))+".csv")
